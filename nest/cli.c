f8bc20a211247a7ae0427c0f1e702b328f36962b
Katerina Kubecova <katerina.kubecova@nic.cz>
2024-01-11 08:57:50 +0100

    show protocols all in cbor works, added header

--- nest/cli.c
+++ nest/cli.c
@@ -399,6 +399,7 @@ cli_kick(cli *c)
 uint
 yi_process(uint size, byte *rbuf, byte *tbuf, uint tbsize) {
   log("capacity %i buffer %i", tbsize, tbuf);
+  //TODO
   if (detect_down(size, rbuf))
   {
     order_shutdown(0);
