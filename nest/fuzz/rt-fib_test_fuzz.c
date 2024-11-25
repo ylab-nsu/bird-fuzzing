e4f04ae464dc2106d574433ec8323590c92cfeab
Polina Levchenko <levchenko.pv@yandex.ru>
2024-11-26 01:12:04 +0300

    Add fuzz test build
    To build fuzz test run:
    CC=clang ./configure --prefix=$PWD/out --disable-client
    make
    make fuzz_tests

--- nest/fuzz/rt-fib_test_fuzz.c
+++ nest/fuzz/rt-fib_test_fuzz.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 #include "rt_fib_test_my.h"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size % 5 != 0) {
     return -1;
   }
