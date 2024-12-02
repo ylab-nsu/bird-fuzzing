b29719b62196f4dc314fa8d805e9de0455224e95
Anatoly Savin <a.savin1@g.nsu.ru>
2024-12-02 20:16:18 +0700

    refactoring, add conf file

--- fuzz/bgp_server.py
+++ fuzz/bgp_server.py
@@ -1,38 +1,51 @@
+import json
 import socket
 import time
+import threading
 from scapy.all import *
 from scapy.all import IP
 from bgp import *
 
+# loading configuration from file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# getting parameters from config
+BIRD_CON_NAME = config["BIRD_CON_NAME"]
+BGP_PROTO_NAME = config["BGP_PROTO_NAME"]
+BIRD_ASN_ID = config["BIRD_ASN_ID"]
+HOST_BGP_ID = config["HOST_BGP_ID"]
+PARAM_HOLD_TIME = config["PARAM_HOLD_TIME"]
+BIRD_BGP_ID = config["BIRD_BGP_ID"]
+BIRD_BGP_PORT = config["BIRD_BGP_PORT"]
+
 def p_display(pkt):
     if IP in pkt:
-        src_ip = pkt[IP].src  # Извлекаем IP-адрес источника
-        print(f"Source IP: {src_ip}")
-        if pkt[IP].src == '172.18.0.2':
+        src_ip = pkt[IP].src  # getting source's IP-address 
+        if pkt[IP].src == BIRD_BGP_ID:
             if str(pkt.summary()).find("BGPHeader") > 0:
                 if pkt[BGPHeader].type == 1:
-                    send(IP(dst=pkt[IP].src)/TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1,seq=pkt[TCP].ack, flags="PA")/BGPHeader(type=1)/BGPOpen(version=4,AS=65002,hold_time=180,bgp_id='172.18.0.1'))
+                    send(IP(dst=pkt[IP].src)/TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1, seq=pkt[TCP].ack, flags="PA")/BGPHeader(type=1)/BGPOpen(version=4, AS=BIRD_ASN_ID, hold_time=PARAM_HOLD_TIME, bgp_id=HOST_BGP_ID))
                     return "Open Message sent"
                 elif pkt[BGPHeader].type == 2:
-                    send(IP(dst=pkt[IP].src)/TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1,seq=pkt[TCP].ack, flags="PA") / BGPHeader(type=4, len=19))
+                    send(IP(dst=pkt[IP].src)/TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1, seq=pkt[TCP].ack, flags="PA") / BGPHeader(type=4, len=19))
                     return "Update reply sent"
                 elif pkt[BGPHeader].type == 3:
                     return "Notification Received"
                 else:
                     # sending regular bgp keep_alive
-                    send(IP(dst=pkt[IP].src) / TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1,seq=pkt[TCP].ack, flags="PA") / BGPHeader(type=4, len=19))
+                    send(IP(dst=pkt[IP].src) / TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport, ack=pkt[TCP].seq + 1, seq=pkt[TCP].ack, flags="PA") / BGPHeader(type=4, len=19))
                     return "Keep_alive sent"
 
-
-# Запускаем захват пакетов в отдельном потоке
+# Start sniffing packets
 sniffer_thread = threading.Thread(target=lambda: sniff(iface='br-7895e720a1ae', prn=p_display, store=0))
 sniffer_thread.start()
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('172.18.0.1', 179))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST_BGP_ID, BIRD_BGP_PORT))
 s.listen(1)
 conn, addr = s.accept()
 print('Connected by', addr)
 
-#closing off the connection
+# closing off the connection
 conn.close()
--- fuzz/fuzz_open.py
+++ fuzz/fuzz_open.py
@@ -1,46 +1,53 @@
 import time
 from boofuzz import *
 from scapy.all import *
+import json
+import random
+
+# Loading configuration from file
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+BIRD_CON_NAME = config['BIRD_CON_NAME']
+BGP_PROTO_NAME = config['BGP_PROTO_NAME']
+BIRD_ASN_ID = config['BIRD_ASN_ID']
+HOST_BGP_ID = config['HOST_BGP_ID']
+PARAM_HOLD_TIME = config['PARAM_HOLD_TIME']
+BIRD_BGP_ID = config['BIRD_BGP_ID']
+BIRD_BGP_PORT = config['BIRD_BGP_PORT']
+
 
 def restart_uplink(target=None, fuzz_data_logger=None, session=None, sock=None):
+    """Function for restarting BGP protocol in container with bird"""
     try:
-        subprocess.run(['docker', 'exec', 'bird1a', 'birdc', 'restart', 'uplink'], check=True)
-        print("Uplink перезапущен.")
+        subprocess.run(['docker', 'exec', BIRD_CON_NAME, 'birdc', 'restart', BGP_PROTO_NAME], check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Ошибка при перезапуске uplink: {e}")
+        print(f"Failed to restart {BGP_PROTO_NAME}: {e}")
 
 log_buffer = []
 
 def print_new_logs(target=None, fuzz_data_logger=None, session=None, sock=None):
-    global log_buffer
+    """Function for printing logs bird"""
     try:
-        # Чтение последней строки из файла логов
+        # Reading last string from logs file
         process = subprocess.Popen(
-            ['docker', 'exec', '-i', 'bird1a', 'tail', '-n1', '/var/log/bird.log'],
+            ['docker', 'exec', '-i', BIRD_CON_NAME, 'tail', '-n1', '/var/log/bird.log'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        output, _ = process.communicate()  # Читаем последнюю строку
+        output, _ = process.communicate()  # Reading last string
         log_entry = output.decode().strip()
-        if log_entry:  # Если строка не пустая, выводим её
-            print("Новая запись в логах:")
+        if log_entry:  # If string not empty print it
             print(log_entry)
     except Exception as e:
-        print(f"Ошибка при чтении логов контейнера Bird: {e}")
-
-
-
+        print(f"Failed to read logs of container with bird{e}")
 
 def ip_str_to_bytes(ip):
-    """Преобразование IP-адреса в байты."""
+    """Transformation IP-address to bytes."""
     return int.from_bytes(socket.inet_aton(ip), 'big')
 
 def fuzz_open_and_keepalive(dst_ip, dst_port):
-    """Фаззинг Open Message с последующим Keepalive."""
-    PARAM_ASN_ID = 65002
-    PARAM_BGP_ID = "172.18.0.1"
-    PARAM_HOLD_TIME = 180
-
+    """Fuzz Open Message with Keepalive."""
     session = Session(
         target=Target(
             connection=TCPSocketConnection(host=dst_ip, port=dst_port)
@@ -58,9 +65,9 @@ def fuzz_open_and_keepalive(dst_ip, dst_port):
         s_block_end()
         if s_block_start('Open'):
             s_byte(value=0x04, endian=BIG_ENDIAN, name='version', fuzzable=False)
-            s_word(value=PARAM_ASN_ID, endian=BIG_ENDIAN, name='ASN', fuzzable=False)
+            s_word(value=BIRD_ASN_ID, endian=BIG_ENDIAN, name='ASN', fuzzable=False)
             s_word(value=PARAM_HOLD_TIME, endian=BIG_ENDIAN, name='Hold Time', fuzzable=False)
-            s_dword(value=ip_str_to_bytes(PARAM_BGP_ID), endian=BIG_ENDIAN, name='BGP Identifier', fuzzable=False)
+            s_dword(value=ip_str_to_bytes(HOST_BGP_ID), endian=BIG_ENDIAN, name='BGP Identifier', fuzzable=False)
             s_byte(value=b'\xff', endian='>', name='Non-Ext OP Len', fuzzable=False)
             s_byte(value=b'\xff', endian='>', name = 'Non-Ext OP Type', fuzzable=False)
             s_size(block_name='Optional Parameters', length=2, name='Extended Opt. Parm Length', endian=BIG_ENDIAN, fuzzable=False)
@@ -86,11 +93,11 @@ def fuzz_open_and_keepalive(dst_ip, dst_port):
             pass
         s_block_end()
     s_block_end()
-    # Соединение Open -> Keepalive
+    # Connection Open -> Keepalive
     session.connect(s_get("bgp_open"))
     session.connect(s_get("bgp_open"), s_get("bgp_keepalive"))
     session.fuzz()
 
 
-# Выполнение фаззинг-теста
-fuzz_open_and_keepalive('172.18.0.2', 179)
+# Fuzzing test
+fuzz_open_and_keepalive(BIRD_BGP_ID, BIRD_BGP_PORT)
