826e826ce6b3b00f1b81622f08b3460699fae53d
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-12-02 22:41:42 +0700

    linking fix

--- nest/fuzz/rt-fib_test_fuzz.c
+++ nest/fuzz/rt-fib_test_fuzz.c
@@ -1,10 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
-// #include "rt_fib_test_my.h"
-#include "test/birdtest.h"
-#include "test/bt-utils.h"
-
-#include "nest/route.h"
+#include "rt_fib_test_my.h"
 
 int state = 0;
 
@@ -18,14 +14,13 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     bt_config_parse(BT_CONFIG_SIMPLE);
     state = 1;
   } 
-  //bt_test_suite(t_match_random_net, "Testing random prefix matching");
 
-  // //bt_init(1, "./obj/nest/fuzz/rt-fib");
-  // if ((Size % 5 != 0) || (Size == 0)) {
-  //   return -1;
-  // }
-  // //bt_exit_value();
-  return 0;
+  //bt_init(1, "./obj/nest/fuzz/rt-fib");
+  if ((Size % 5 != 0) || (Size == 0)) {
+    return -1;
+  }
+  //bt_exit_value();
+  return t_match_random_net(Data, Size);
 }
 
 
