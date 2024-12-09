4dfc5b6ba0f22d9f3898ef701d6eeeda814b4d33
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-12-09 20:51:06 +0700

    add stderr ignoring

--- nest/fuzz/build.sh
+++ nest/fuzz/build.sh
@@ -13,44 +13,59 @@ should_skip() {
     return 1 
 }
 
-should_hide_stdin() {
+should_hide_stdout() {
     for hide_flag in "${SKIP_STEPS[@]}"; do
-        if [[ "$hide_flag" == "stdin" ]]; then
+        if [[ "$hide_flag" == "stdout" ]]; then
             return 0  
         fi
     done
     return 1 
 }
 
+should_hide_stderr() {
+    for hide_flag in "${SKIP_STEPS[@]}"; do
+        if [[ "$hide_flag" == "stderr" ]]; then
+            return 0  
+        fi
+    done
+    return 1 
+}
+
+redirect_output() {
+    local command=$1
+    local stdout_redirect=""
+    local stderr_redirect=""
+
+    if should_hide_stdout; then
+        stdout_redirect="1>/dev/null"
+    fi
+
+    
+    if should_hide_stderr; then
+        stderr_redirect="2>/dev/null"
+    fi
+
+    eval "$command $stdout_redirect $stderr_redirect"
+}
+
+
 if ! should_skip 1; then
     echo "STEP_1: CC=clang ./configure --prefix=$PWD/out --disable-client"
-    if should_hide_stdin; then 
-        CC=clang ./configure --prefix=$PWD/out --disable-client 1>/dev/null
-    else 
-        CC=clang ./configure --prefix=$PWD/out --disable-client
-    fi
+    redirect_output "CC=clang ./configure --prefix=$PWD/out --disable-client"
 else
     echo "STEP_1: SKIP"
 fi
 
 if ! should_skip 2; then
     echo "STEP_2: make"
-    if should_hide_stdin; then 
-        make 1>/dev/null
-    else 
-        make
-    fi
+    redirect_output "make"
 else
     echo "STEP_2: SKIP"
 fi
 
 if ! should_skip 3; then
     echo "STEP_3: make fuzz_tests"
-    if should_hide_stdin; then
-        make fuzz_tests 1>/dev/null
-    else
-        make fuzz_tests
-    fi
+    redirect_output "make fuzz_tests"
 else
     echo "STEP_3: SKIP"
 fi
