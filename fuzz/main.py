4d2be1fb1cb2e38a70d4a2dedce153bb2721ba1b
Anatoly Savin <a.savin1@g.nsu.ru>
2024-12-16 23:51:44 +0700

    add test for notification

--- fuzz/main.py
+++ fuzz/main.py
@@ -1,5 +1,7 @@
 import json
 from bgp_fuzz_open_message import BGPFuzzOpenMessage
+from bgp_fuzz_notification_message import BGPFuzzNotificationMessage
+
 
 
 # Fuzzing Open Message
@@ -9,7 +11,10 @@ bgp_test.fuzz_bgp_open_optional_param_length()
 #bgp_test.fuzz_bgp_open_random_params()
 #bgp_test.fuzz_bgp_open_version_field()
 #bgp_test.fuzz_bgp_open_length_mismatch()
-#bgp_test.fuzz_open_asn()
+#bgp_test.fuzz_open_asn()s
 #bgp_test.fuzz_open_hold_time()
 #bgp_test.fuzz_open_identifier()
 #bgp_test.fuzz_open_version_length()
+
+bgp_test2 = BGPFuzzNotificationMessage('config.json')
+#bgp_test2.fuzz_notification()
