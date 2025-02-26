27f63a303bb840542151a3689f57c76626f5f7e9
Ruslan <r.chudinov@g.nsu.ru>
2025-02-27 00:55:54 +0700

    Fix bad output logs.

---
+++ nest/fuzz/test.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stddef.h>
+
+
+int FuzzMe(const uint8_t *Data, size_t DataSize) {
+  return DataSize >= 3 &&
+      Data[0] == 'F' &&
+      Data[1] == 'U' &&
+      Data[2] == 'Z' &&
+      Data[3] == 'Z';  // :‑<
+      
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  FuzzMe(Data, Size);
+  return 0; 
+
+}
